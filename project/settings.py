edef6372dc94df92ac5add47ad623ece10f16b1d
darkdrei <eugenio.pajaro.ext@nokia.com>
2018-09-21 19:53:06 -0500

    update str in models schedule

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8cyg7t8nz3&^_&)!w9p#-9qx4tpd6cz#ta%k*#6s1g+6-a6^gj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
