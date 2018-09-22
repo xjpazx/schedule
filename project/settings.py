5590b399e2c4f8985f5f9c54d142b94a66a89b3d
darkdrei <eugenio.pajaro.ext@nokia.com>
2018-09-21 19:50:19 -0500

    update str in models schedule

--- project/settings.py
+++ project/settings.py
@@ -135,10 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATIC_URL = '/static/'
 STATIC_ROOT = '/home/ubuntu/pro_schedule/schedule/static/'
 MEDIA_URL = '/media/'
 HOST_MEDIA = '/home/ubuntu/pro_schedule/schedule/media/'
