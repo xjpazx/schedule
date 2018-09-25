1c6a46fbabdde01eda22b9f7f1899a087a2efaf0
JOSEPH <pazgenes@hotmail.com>
2018-09-24 19:35:53 -0500

    validando time

--- schedule/forms.py
+++ schedule/forms.py
@@ -23,13 +23,10 @@ class ActivityForm(forms.ModelForm):
 
     def clean(self):
         data = self.cleaned_data
-        try:
-            start_time = data['start_time']
-            end_time = data['end_time']
-            if start_time > end_time:
-                raise ValidationError('The start time must be less than the end time')
-        except:
-            raise ValidationError('The start time and the end time must have the following format HH:MM')
+        start_time = data['start_time']
+        end_time = data['end_time']
+        if start_time > end_time:
+            raise ValidationError('The start time must be less than the end time')
         return self.cleaned_data
 
 
