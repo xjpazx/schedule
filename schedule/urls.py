06b28d5810fc610d1b40808b4eaa1f958155328e
Rafael Barboza <barboza.rafael.p@gmail.com>
2018-10-09 10:58:52 -0500

    adding configuration for django-rest project

---
+++ schedule/urls.py
@@ -0,0 +1,29 @@
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include
+from django.conf.urls.static import static
+admin.site.index_template = 'admin/my_custom_index.html'
+admin.autodiscover()
+urlpatterns = [
+    url(r'^select2/', include('django_select2.urls')),
+    url(r'^api/v1/', include('schedule.urls')),
+    url(r'^', admin.site.urls),
+]  +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
